312fee3d3fcc0d51b594ed3185acc1020f9e71c1
root <Stvnliao@yahoo.com>
2016-11-17 22:20:05 -0500

    Header

--- resources/views/includes/layouts/header.blade.php
+++ resources/views/includes/layouts/header.blade.php
@@ -1,6 +1,8 @@
-<header>
-	<a href="/">
-		<img class="logo-image" src="images/Logo.svg" alt="Steven Liao">
-	</a>
-	{!! navigation($active_page || 'home') !!}
+<header class="background-green">
+	<div class="container">
+		<a href="/">
+			<img class="logo-image" src="images/Logo.svg" alt="Steven Liao">
+		</a>
+		{!! navigation($active_page || 'home') !!}
+	</div>
 </header>
