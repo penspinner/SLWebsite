9f0228b0c7736324ec50d5e35d13f5978148805e
root <Stvnliao@yahoo.com>
2016-11-29 23:44:54 -0500

    Update projects page #

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,8 +1,12 @@
 @extends('includes/layouts/default')
 @section('content')
 <main>
-	<section class="background-desk">
-	
+	<section class="">
+		<div class="info text-center container">
+			<h1 class="margin">My Projects</h1>
+		</div>
 	</section>
+	<!-- A section tag for each project	 -->
+	<section></section>
 </main>
 @stop
