bb2d526550e99c29ed3b2621582da8bc8c6d1d2f
root <Stvnliao@yahoo.com>
2016-11-30 18:36:55 -0500

    Update projects page #3

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -11,10 +11,15 @@
 	<div class="projects">
 		<!-- A section tag for each project	 -->
 		<section class="background-lightBlue">
-			<div class="container">
-				<h1>
-					Digital Conceptions
-				</h1>
+			<div class="container text-center">
+				<h2>Digital Conceptions</h2>
+				<h4>Lead Programmer</h4>
+				<a href="http://digital-conceptions-6.appspot.com/">
+					<img class="project-image" src="images/projects/DigitalConceptions.png" alt="Digital Conceptions">
+				</a>
+				<p>
+					Description
+				</p>
 			</div>
 		</section>
 	</div>
