f7ecb1ebe97b09c087fe71191de79366bb821009
root <Stvnliao@yahoo.com>
2016-11-29 23:56:06 -0500

    Update projects page #2

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -6,7 +6,17 @@
 			<h1 class="margin">My Projects</h1>
 		</div>
 	</section>
-	<!-- A section tag for each project	 -->
-	<section></section>
+	
+	<!-- Beginning of project listings	 -->
+	<div class="projects">
+		<!-- A section tag for each project	 -->
+		<section class="background-lightBlue">
+			<div class="container">
+				<h1>
+					Digital Conceptions
+				</h1>
+			</div>
+		</section>
+	</div>
 </main>
 @stop
